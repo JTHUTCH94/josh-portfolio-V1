d9e553338a5c1effcc6a995a9534843fa8f02981
Joshua Hutchinson <jthutch94@gmail.com>
2018-05-13 14:32:25 -0400

    deployment attempt

--- portfolio-v1/src/App.js
+++ portfolio-v1/src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
 import Contact from './components/Contact';
 import Projects from './components/Projects';
