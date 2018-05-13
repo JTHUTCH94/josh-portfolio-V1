5590e2c976414efe681678b4eca690f6c0a81d3f
Joshua Hutchinson <jthutch94@gmail.com>
2018-05-13 14:28:13 -0400

    tryin to deploy

--- portfolio-v1/src/components/AboutMe.js
+++ portfolio-v1/src/components/AboutMe.js
@@ -6,4 +6,4 @@ export default function AboutMe() {
             <p>My name is Joshua Hutchinson and I am beginning my career as a Junior Web Developer. By partaking in Thinkful's full-time engineering immersion program I have acquired a skill set that allows me to build mobile-first web based applications using a full stack of development languages. My current skill set includes JavaScript, HTML5, CSS3, and React.js, amongst a few other Middleware based languages such as Express. I am currently in search of an empolyment opportunity in an environment that will allow me to further continue my education and improve upon my skillset.</p>
         </div>
     )
-}
+};
